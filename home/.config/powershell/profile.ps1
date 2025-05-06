505ab13308ae40cf6f86f6d27c673e753aeb8c67
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2025-05-06 06:34:19 -0400

    Update PowerShell configuration

--- home/.config/powershell/profile.ps1
+++ home/.config/powershell/profile.ps1
@@ -24,7 +24,7 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock `
 {
     param($wordToComplete, $commandAst, $cursorPosition)
 
-    dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object
+    dotnet complete --position $cursorPosition "$commandAst" | ForEach-Object `
     {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
